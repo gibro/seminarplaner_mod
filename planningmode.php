459973bb9ca9a5dd4721d52771691bffd5c4bf31
guido.brombach@igmetall.de <guido.brombach@igmetall.de>
2026-07-20 12:32:19 +0200

    Einreichungsreife: GPL-Header, LICENSE, Abhaengigkeit, 1.0.0 stable
    Vorbereitung fuer die Einreichung im Moodle Marketplace:
    
    - Vollstaendiger Moodle-GPL-Boilerplate-Header in allen 57 PHP-Dateien
      (bisher nur eine Datei mit unvollstaendigem Header), dazu je ein
      File-Level-Docblock mit @package, @copyright und @license.
    - LICENSE (GPLv3-Volltext) ergaenzt; lag bisher nur beim Local-Plugin.
    - $plugin->dependencies auf local_seminarplaner deklariert. Die Aktivitaet
      liest die Methodenset-Tabellen des Local-Plugins und ist ohne dieses
      nicht lauffaehig; bisher war die Abhaengigkeit nur implizit.
    - maturity MATURITY_RC -> MATURITY_STABLE, release 1.0.0-rc1 -> 1.0.0,
      version 2026072000.
    
    Keine funktionalen Aenderungen. Alle 57 Dateien mit php -l geprueft.

--- planningmode.php
+++ planningmode.php
@@ -1,12 +1,26 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
-// D16/D46: Der eigene „Bausteine"-Planungsmodus ist entfallen – Bausteine
-// werden jetzt direkt in der Sequenzansicht geplant. Alte Links und
-// Lesezeichen (u. a. der Überblick verlinkt noch hierher) landen daher
-// stillgelegt in der Sequenz. Die Datenmigration alter Pläne bleibt davon
-// unberührt (sie läuft über die Upgrade-Schritte und den Grid→Sequenz-
-// Konverter, nicht über diese Seite).
+/**
+ * Planningmode.
+ *
+ * @package    mod_seminarplaner
+ * @copyright  2026 Guido Brombach <gibro@posteo.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 require_once(__DIR__ . '/bootstrap.php');
 require_once(__DIR__ . '/locallib.php');
